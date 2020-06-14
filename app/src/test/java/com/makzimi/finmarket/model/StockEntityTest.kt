8f4bae0887f2d669c2fcad2f89164f23c098a66e
Maxim Kachinkin <makzimi@gmail.com>
2020-06-14 22:14:42 +0300

    Commit junit dependencies.

---
+++ app/src/test/java/com/makzimi/finmarket/model/StockEntityTest.kt
@@ -0,0 +1,19 @@
+package com.makzimi.finmarket.model
+
+import org.junit.jupiter.api.Test
+
+class StockEntityTest {
+
+    @Test
+    internal fun when_correctStockApiEntity_then_correctStockEntity() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    @Test
+    internal fun when_correctCompanyStockEntity_then_correctStockEntity() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
+
